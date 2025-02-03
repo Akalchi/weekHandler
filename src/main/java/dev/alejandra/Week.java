cba214035f8fea6b1ebb4de756265ab0e3a39781
Akalchi <alejandrafdez1699@gmail.com>
2025-02-03 14:50:59 +0100

    "feat [getDaysOfWeek] implementing array list and get das of week"

--- src/main/java/dev/alejandra/Week.java
+++ src/main/java/dev/alejandra/Week.java
@@ -1,9 +1,22 @@
 package dev.alejandra;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Week {
     private List<String> days;
     public Week() {
+        this.days = new ArrayList<>();
+        this.days.add("Monday");
+        this.days.add("Tuesday");
+        this.days.add("Wednesday");
+        this.days.add("Thursday");
+        this.days.add("Friday");
+        this.days.add("Saturday");
+        this.days.add("Sunday");
+    }
+
+    public List<String> getDaysOfWeek() {
+        return this.days;
     }
 }
